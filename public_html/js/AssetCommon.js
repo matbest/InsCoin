f9b3b3b16272002d120ba99388bb62cdae92abc9
Mathew <accounts@mathewbest.com>
2018-06-16 22:52:36 +0100

    assetbrowser page first pass (without functionality

--- public_html/js/AssetCommon.js
+++ public_html/js/AssetCommon.js
@@ -12,3 +12,38 @@ function consoleErrorLog(thisString)
   var html = thisString + "<br>" + oldcontent;
   document.getElementById("console").innerHTML = html;
 }
+
+function GetMintPrice(func)
+{
+  //var plotContract = loadContract(contractAddress, ContractABI);
+  var contract = EthereumConnection.GetContract();
+
+  contract.GetMintPriceInWEI.call().then(function(a)
+  {
+    //console.log(a.toString(10));
+    price = Number(a[0]);
+    price /=ethinwei;
+    func(price.toString())
+  });
+}
+
+function myTest()
+{
+
+  var contract = EthereumConnection.GetContract("IPFS");
+  contract.GetMintPriceInWEI(function(err, res){
+    if (err)
+    {
+        document.getElementById("testbutton").innerHTML = "Not Connected to Ethereum Error!";
+    }
+    else
+    {
+      price = Number(res[0]);
+      if (price == 10000000000000000)
+        document.getElementById("testbutton").innerHTML = "OK!";
+        else {
+          document.getElementById("testbutton").innerHTML = "C Error!";
+        }
+    }
+  });
+}
