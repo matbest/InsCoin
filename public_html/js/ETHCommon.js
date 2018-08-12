cf9a85286e5334726f77a1e1896f0462318522e9
Mathew <accounts@mathewbest.com>
2018-08-12 20:56:13 +0100

    updated ropsten address

--- public_html/js/ETHCommon.js
+++ public_html/js/ETHCommon.js
@@ -10,7 +10,7 @@ ethinwei = 1000000000000000000; // This is 1 ETH
 // use remix to deploy.
 var contractAddresses = {
   'IPFS-Ganache': '0xef53e2daec57472c517f4e30da3126e1e6536277',
-  'IPFS-Ropsten': '0xe1a6ffeab066405e56f3f09fd550589bd3674ab6',
+  'IPFS-Ropsten': '0xd1aca61bc4e32b7a841cb5caff17565110b97993',
     'DLand-Ganache': '0xcaf22b02250339642e73e6a3a2d9dc53653a30e8',
     'DLand-Ropsten': '0x39d059590ea9defb8574f3f2e2fb2447ea05515a'
 };
